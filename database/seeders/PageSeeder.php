4887ea3540b2eb2030fb4783e17a9efc29b8140a
ADitta <ADitta@users.noreply.github.com>
2026-03-13 09:35:16 +0000

    bug fix for page seeder url

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -16,7 +16,7 @@ class PageSeeder extends Seeder
             ['id' => 1],
             [
                 'title' => 'Welcome to my LSi Coding Exercise solution',
-                'content' => '<p>This is my submission where I added the following features. </p><ul><li>Multiple images linked to products</li><li>URL slug for products</li><li>Enquiry form that leads to a preview page</li><li>Responsive styling with tailwind and DaisyUI</li></ul> <p> You can visit the project at: <a class="link" href="https://github.com/scott-lsi/exercise">https://github.com/ADitta/exercise</a>.</p><p>Thank you for visiting!</p>',
+                'content' => '<p>This is my submission where I added the following features. </p><ul><li>Multiple images linked to products</li><li>URL slug for products</li><li>Enquiry form that leads to a preview page</li><li>Responsive styling with tailwind and DaisyUI</li></ul> <p> You can visit the project at: <a class="link" href="https://github.com/ADitta/exercise">https://github.com/ADitta/exercise</a>.</p><p>Thank you for visiting!</p>',
             ]
         );
     }
