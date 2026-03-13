886ba69e748e76e9e9aaee260a2eaeaf521757cc
ADitta <ADitta@users.noreply.github.com>
2026-03-13 06:38:28 +0000

    add slug column to product table/populate seeder and change url to use slug

--- resources/views/components/product-grid-item.blade.php
+++ resources/views/components/product-grid-item.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('product.show', $product) }}">
+<a href="/products/{{ $product->slug }}">
     <img src="{{ asset('images/product/' . $product->image) }}" alt="{{ $product->name }}">
     {{ $product->name }}
 </a>
